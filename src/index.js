fc52e7a8c574ae84eada2f2ca8dd68c72aa8c0ab
Jan <kelevra.1337@gmail.com>
2020-12-03 22:33:28 +0100

    added bootstrap and sass compiler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// import bootstrap css to project
+import './bootstrapCustom.scss';
+
+
 // initialize firebase 
 // before doing any app stuff....
 import './components/firebase';
