dbcc64507ece4125500dc735690734a2d35cef49
Jan <kelevra.1337@gmail.com>
2020-12-03 22:51:14 +0100

    added bootstrap container

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import firebase from 'firebase/app';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap'
 
 import { userContext } from './userContext';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import * as ROUTES from '../config/routes';
 
@@ -12,6 +13,7 @@ import Landing from './Landing';
 import SignUp from './Auth/SignUp';
 import SignIn from './Auth/SignIn';
 import Home from './Home';
+
 
 
 const login = () => {
@@ -28,25 +30,26 @@ function App() {
 
 
   return (
-    <userContext.Provider 
+    <userContext.Provider
       value={{
-        user:user,
-        loading
-    }}>
+        user: user,
+        loading,
+        error
+      }}>
       <Router>
         <Navigation />
-        <hr />
-        <p>
-          Current User: {user?.email} - {user?.uid}
-        </p>
-        <button onClick={login}>Login</button>
-        <button onClick={logout}>Logout</button>
-        {/* Define the routes here */}
-        <Route exact path={ROUTES.LANDING} component={Landing} />
-        <Route path={ROUTES.SIGN_UP} component={SignUp} />
-        <Route path={ROUTES.SIGN_IN} component={SignIn} />
-        <Route path={ROUTES.HOME} component={Home} />
-
+        <Container>
+          <p>
+            Current User: {user?.email} - {user?.uid}
+          </p>
+          <button onClick={login}>Login</button>
+          <button onClick={logout}>Logout</button>
+          {/* Define the routes here */}
+          <Route exact path={ROUTES.LANDING} component={Landing} />
+          <Route path={ROUTES.SIGN_UP} component={SignUp} />
+          <Route path={ROUTES.SIGN_IN} component={SignIn} />
+          <Route path={ROUTES.HOME} component={Home} />
+        </Container>
       </Router>
     </userContext.Provider>
   );
