ab1ffc6fd713a88d75b28292309c2cf261bcf59a
Jan <kelevra.1337@gmail.com>
2020-12-03 22:33:48 +0100

    conditional rendering for the navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,37 +1,38 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { signOut } from './Auth/helper';
 
+
+import { Link } from 'react-router-dom';
+import { signOut, useSession } from './Auth/helper';
 
 import * as ROUTES from '../config/routes';
+import { Nav, Navbar } from 'react-bootstrap';
 
 
-const Navigation = () => (
-  <div>
-    <ul>
-      <li>
-        <Link to={ROUTES.SIGN_IN}>Anmelden</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.SIGN_UP}>Registrieren</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.LANDING}>Landing</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.HOME}>Home</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ACCOUNT}>Account</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ADMIN}>Admin</Link>
-      </li>
-      <li>
-        <button onClick={signOut}>Abmelden</button>
-      </li>
-    </ul>
-  </div>
-)
+const Navigation = () => {
+  const user = useSession();
+
+  
+  const nonAuthLinks = <React.Fragment>
+    <Nav.Link as={Link} to={ROUTES.SIGN_IN}>Anmelden</Nav.Link>
+    <Nav.Link as={Link} to={ROUTES.SIGN_UP}>Registrieren</Nav.Link>
+  </React.Fragment>
+
+  const authLinks = <React.Fragment>
+    <Nav.Link as={Link} to={ROUTES.HOME}>Home</Nav.Link>
+    <Nav.Link as={Link} to={ROUTES.ACCOUNT}>Account</Nav.Link>
+    <Nav.Link as={Link} to={ROUTES.ADMIN}>Admin</Nav.Link>
+    <Nav.Link onClick={signOut}>Abmelden</Nav.Link>
+  </React.Fragment>
+
+  return(
+    <Navbar className="justify-content-end" bg="light" expand="lg">
+      <Nav>
+        <Nav.Link as={Link} to={ROUTES.LANDING}>Landing</Nav.Link>
+        {!user && nonAuthLinks}
+        {user && authLinks}
+      </Nav>
+    </Navbar>
+  )
+}
 
 export default Navigation;
