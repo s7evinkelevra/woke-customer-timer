ca69710d0bcb4974ae63c4089958bd857e49d31e
Jan <kelevra.1337@gmail.com>
2020-12-04 15:44:27 +0100

    migrated sticky form to react bootstrap form

--- src/components/DB/CreateSticky.js
+++ src/components/DB/CreateSticky.js
@@ -3,6 +3,7 @@ import firebase from 'firebase';
 import { useSession } from '../Auth/helper';
 import { getUserFields, createSticky } from './helper';
 import { useForm } from 'react-hook-form';
+import { Button, Form } from 'react-bootstrap';
 
 
 
@@ -19,6 +20,20 @@ const testEntry = async (user) => {
   console.log(entry);
 }
 
+const CustomFormGroup = (props) => {
+
+  return(
+    <Form.Group controlId={props.controlId}>
+      <Form.Label>{props.label}</Form.Label>
+      <Form.Control {...props.control} />
+      <Form.Text className="text-muted">
+        {props.errors[props.title] && <span>{props.errorMessage}</span>}
+      </Form.Text>
+    </Form.Group>
+  )
+}
+
+
 const CreateSticky = (props) => {
   const user = useSession();
   const { register, handleSubmit, watch, errors, getValues } = useForm();
@@ -29,18 +44,54 @@ const CreateSticky = (props) => {
       ...getValues()
     });
   }
-
-
-   const recurringIntervalField = () => (
-    <div>
-      <label htmlFor="recurringInterval">Intervall [Tage]</label>
-      <input type="number" name="recurringInterval" defaultValue={0} ref={register} />
-      {errors.recurringInterval && <span>recurring error lul error</span>}
-    </div>
-  )
-
+  
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Form.Group controlId="formTitle">
+        <Form.Label>Titel</Form.Label>
+        <Form.Control type="text" name="title" placeholder="Titel eingeben" ref={register({required:true})} />
+        <Form.Text className="text-muted">
+          {errors.title && <span>Title is required!</span>}
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId="formDescription">
+        <Form.Label>Beschreibung</Form.Label>
+        <Form.Control as="textarea" type="text" name="description" placeholder="Beschreibung eingeben" ref={register} />
+        <Form.Text className="text-muted">
+          {errors.descriptions && <span>Was da los?</span>}
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId="formDueDate">
+        <Form.Label>Stichtag</Form.Label>
+        <Form.Control type="date" name="dueDate" ref={register} />
+        <Form.Text className="text-muted">
+          {errors.dueDate && <span>Was da los?</span>}
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId="formRecurring">
+        <Form.Check type="checkbox" label="Wiederholen?" name="recurring" ref={register} />
+        <Form.Text className="text-muted">
+          {errors.dueDate && <span>Was da los?</span>}
+        </Form.Text>
+        {/* conditionally render the number input form lul */}
+        {watch("recurring") && 
+          <React.Fragment>
+            <Form.Label>Interval [Tage]</Form.Label>
+            <Form.Control type="number" name="interval" defaultValue="1" min="1" ref={register({min:1})} />
+            {errors.interval && <span>Interval muss mindestens 1 Tag sein!</span>}
+          </React.Fragment>}
+      </Form.Group>
+
+      <Button variant="primary" type="submit">Sticky erstellen!</Button>
+
+
+    </Form>
+
+
+/*     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="title">Titel</label>
         <input name="title" ref={register({ required: true })} />
@@ -72,7 +123,7 @@ const CreateSticky = (props) => {
       {errors.errorMessage?.message}
 
       <input type="submit" />
-    </form>
+    </form> */
   ); 
 };
 
