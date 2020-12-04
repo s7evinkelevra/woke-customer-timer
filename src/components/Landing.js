37d3e33be25a02dad95723306fda143295dfc1b3
Jan <kelevra.1337@gmail.com>
2020-12-04 03:03:36 +0100

    some other stuff

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Button, Jumbotron } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import * as ROUTES from '../config/routes';
 
 const Landing = () => {
   return (
-    <div>
-      Landing page yeee
-    </div>
+    <Jumbotron>
+      <h1>Sticky Sachen machen yee yee</h1>
+      <p>
+        Text zu diesen Sachen ist mir eigentlich egal yee yee
+      </p>
+      <Button as={Link} to={ROUTES.SIGN_IN} variant="primary">Anmelden</Button>
+    </Jumbotron>
   );
 };
 
