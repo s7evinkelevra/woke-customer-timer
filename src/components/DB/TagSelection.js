10971dd544a0f3164cda4a355e1874899cfe2ec0
Jan <kelevra.1337@gmail.com>
2021-03-04 13:12:54 +0100

    moved tags to own file

--- src/components/DB/TagSelection.js
+++ src/components/DB/TagSelection.js
@@ -1,23 +1,9 @@
 import React, { useState } from 'react';
 import _ from 'lodash';
 
+import Tag from './Tag';
 import * as SPECIALTAGS from '../../config/specialTags';
 import { useStickies } from './helper';
-
-
-const Tag = ({ tag, count, active, toggleActive = () => {}, ...props }) => {
-
-  const badgeClasses = `btn badge badge-${active ? "secondary" : "light"} rounded pr-2 pl-2 py-2 mb-2 mr-2`
-
-  return (
-    <a onClick={ () => {toggleActive(tag)} } key={tag} className={badgeClasses}>
-      {tag}
-      {count && 
-        <span className="text-white rounded bg-info px-1 ml-2">{count}</span>}
-    </a>
-  )
-}
-
 
 const TagSelection = (props) => {
   const { stickiesRef, tagFilter, setTagFilter } = useStickies();
@@ -27,8 +13,6 @@ const TagSelection = (props) => {
     const toggledTags = _.xor(tagFilter,[tag])
     setTagFilter(toggledTags);
   }
-
-  console.log(tagFilter);
 
   const stickies = stickiesRef.docs.map((doc) => (doc.data()));
   const tagCounts = _.chain(stickies).map((sticky) => (sticky.tags || [SPECIALTAGS.NOTAG])).flatten().countBy().value();
